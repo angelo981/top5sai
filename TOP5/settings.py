0eca23ad179cbd3345b81e808aa542a4a97ecc2b
angelo981 <angeroshema@gmail.com>
2026-09-17 23:23:13 +0200

    Initial commit

--- TOP5/settings.py
+++ TOP5/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--yhmmq7cdebnj)1um1up=egej7rbu@24=cx2a-j&rq(rib#b7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Allow hosts
